9f539a0f6fa473950952c099bd4b501d579c10f6
godnew <1018020732@qq.com>
2017-07-17 16:57:08 +0800

    头部模块

---
+++ src/components/tabs.vue
@@ -0,0 +1,63 @@
+/**
+* Created by godnew on 2017/7/17.
+*/
+
+
+<template>
+    <div>
+      <ul>
+        <li v-for="(item,index) in list" @click.stop="change(index)" :class="{active:index==nowIndex}">{{item}}</li>
+      </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+              nowIndex:0
+            }
+        },
+        props:{
+            list:{
+              type: Array,
+              default: [1,2,3],
+              required: true
+            }
+        },
+        methods:{
+            change(index){
+                this.nowIndex=index;
+            }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+  ul{
+    display: flex;
+    justify-content: space-between;
+    li{
+      position: relative;
+      height: 50px;
+      line-height: 50px;
+      font-size: 14px;
+      flex: 1;
+      text-align: center;
+      cursor: pointer;
+      color:#999;
+    }
+    .active{
+      color: #ff7777;
+      &::after{
+        width: 100%;
+        height: 2px;
+        content: '';
+        background: #ff7777;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+      }
+    }
+  }
+</style>
---
+++ src/views/layout.vue
@@ -0,0 +1,33 @@
+/**
+* Created by godnew on 2017/7/17.
+*/
+
+<template>
+    <div>
+      <header>{{title}}</header>
+      <tabs :list="['流行','歌单','排行榜','热门歌手']"></tabs>
+    </div>
+</template>
+
+<script>
+    import Tabs from "@/components/tabs"
+    export default {
+      components:{Tabs},
+      data(){
+          return {
+              title:'云音乐'
+          }
+      }
+    }
+</script>
+
+<style scoped lang="less">
+header{
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #ff7777;
+  color: #fff;
+}
+</style>
